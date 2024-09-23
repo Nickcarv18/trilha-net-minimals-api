968788953c6e6b59dd9d45e19bf739d5b6bee770
Nicoli Carvalho <nicoli.pereira@e-construmarket.com.br>
2024-09-23 19:13:43 -0300

    Criação da tabela de administradores e seed para cadastro padrão

--- Infraestrutura/DbContexto.cs
+++ Infraestrutura/DbContexto.cs
@@ -2,11 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using trilha_net_minimals_api.Dominio.Entidades;
 
 namespace trilha_net_minimals_api.Infraestrutura
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+
+        private readonly IConfiguration _configurationAppSettings;
+
+        public DbContexto(IConfiguration configurationAppSettings)
+        {
+            _configurationAppSettings = configurationAppSettings;
+        }
+
+        public DbSet<Administrador> Administradores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrador>().HasData(
+                new Administrador{
+                    Id = 1,
+                    Email = "adm@admTeste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                }
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured){
+                var stringConexao = _configurationAppSettings.GetConnectionString("sqlServer");
+
+                if(!string.IsNullOrEmpty(stringConexao)){
+                    optionsBuilder.UseSqlServer(stringConexao);
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using trilha_net_minimals_api.Dominio.DTOs;
+using trilha_net_minimals_api.Infraestrutura;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlServer"))
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
